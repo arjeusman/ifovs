0987bd21bc6c20c9ca4e5a8daa7fc9fd9a81db1f
Arje Usman <arjeusman24@gmail.com>
2023-12-02 00:39:47 +0800

    modified:   dashboard/components/header.php 	modified:   dashboard/signin.php

--- dashboard/signin.php
+++ dashboard/signin.php
@@ -38,7 +38,7 @@ endif;
   <title>iFovs</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"crossorigin="anonymous"/>
   <link rel="icon" href="assets/favicon.ico">
-  <link rel="stylesheet" type="text/css" href="assets/dark.css" />
+  <link rel="stylesheet" type="text/css" href="assets/bootstrap.theme.css" />
   <link rel="stylesheet" type="text/css" href="assets/style.css"/>
   <?php include('includes/popMessage.php'); ?>
 </head>
